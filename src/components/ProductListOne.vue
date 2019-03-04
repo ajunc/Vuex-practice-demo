56d1c990d7626467d09cf57cb78812ab1d7938d7
benjun_xu <894012405@qq.com>
2019-03-04 11:54:56 +0800

    添加state、getters、mutations、actions、babel-preset-stage-2插件

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -2,21 +2,41 @@
   <div id="product-list-one">
     <h2>Product List One</h2>
     <ul>
-        <li v-for="(product,index) in products" :key="index">
+        <li v-for="(product,index) in saleProducts" :key="index">
             <span class="name">{{product.name}}</span>
             <span class="price">{{product.price}}</span>
         </li>
     </ul>
+    <button @click="reducePrice(4)">商品降价</button>
   </div>
 </template>
 
 <script>
+import {mapGetters} from "vuex";
+import {mapActions} from "vuex";
 export default {
-  computed: {
-      products() {
-          return this.$store.state.products;
-      }
-  }
+    computed: {
+        products() {
+            return this.$store.state.products;
+        },
+        //saleProducts() {
+            //return this.$store.getters.saleProducts;
+        //}
+        ...mapGetters([
+            "saleProducts"
+        ])
+    },
+    methods:{
+        // reducePrice(amout){
+        //     //this.$store.commit('reducePrice');
+
+        //     this.$store.dispatch("reducePrice",amout);
+        // }
+
+        ...mapActions([
+            "reducePrice"
+        ])
+    }
 }
 </script>
 
--- src/store/store.js
+++ src/store/store.js
@@ -4,11 +4,41 @@ import Vuex from "vuex";
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
+    strict: true,
     state: {
         products: [
             { name: "马云", price: 200 },
             { name: "马化腾", price: 145 },
             { name: "马已", price: 100 }
           ]
+    },
+    getters:{
+        saleProducts:(state) => {
+            var saleProducts = state.products.map(
+                product => {
+                    return {
+                        name: "**" + product.name + "**",
+                        price: product.price / 2
+                    }
+                }
+            );
+            return saleProducts;
+        }
+    },
+    mutations:{
+        reducePrice: (state,payload) => {
+            //setTimeout(function(){
+                state.products.forEach(product => {
+                    product.price -= payload;
+                })
+            //})
+        }
+    },
+    actions: {
+        reducePrice: (context,payload) => {
+            setTimeout(function(){
+                context.commit("reducePrice",payload)
+            },2000)
+        }
     }
 })
